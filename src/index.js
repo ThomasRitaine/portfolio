c2388329ccc1c2d1c6f9a28a55c5210f84f939c1
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-09-27 00:28:23 +0200

    feat(Tracking): :sparkles: Add Google Analytics v4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./i18n/config";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-6SMSN52814");
 
 ReactDOM.render(
   <React.StrictMode>
