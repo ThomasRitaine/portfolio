baffcce83f31c70fe5fb0bd889c6114a8304e8f0
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-06-21 18:52:31 +0200

    feat(Projects): Add projects and rewrite some project description

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,8 @@ import pipeline from "../../Assets/Projects/ci-cd-pipeline.webp";
 import portfolio from "../../Assets/Projects/portfolio.webp";
 import terminal from "../../Assets/Projects/terminal.webp";
 import etuuttApiSymfony from "../../Assets/Projects/etuutt-api-symfony.webp";
+import cdn from "../../Assets/Projects/cdn.webp";
+import chatdoc from "../../Assets/Projects/chatdoc.webp";
 import { Trans, withTranslation } from "react-i18next";
 
 function Projects() {
@@ -21,24 +23,25 @@ function Projects() {
           <Trans>projects.body</Trans>
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={server}
+              imgPath={chatdoc}
               isBlog={false}
-              title={<Trans>projects.vps.title</Trans>}
-              description={<Trans>projects.vps.body</Trans>}
-              ghLink="https://github.com/ThomasRitaine/server-config"
+              title={<Trans>projects.chatdoc.title</Trans>}
+              description={<Trans>projects.chatdoc.body</Trans>}
+              ghLink="https://github.com/ThomasRitaine/chatdoc"
+              demoLink="https://chatdoc.thomas.ritaine.com"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={pipeline}
+              imgPath={cdn}
               isBlog={false}
-              title={<Trans>projects.ci-cd.title</Trans>}
-              description={<Trans>projects.ci-cd.body</Trans>}
+              title={<Trans>projects.cdn.title</Trans>}
+              description={<Trans>projects.cdn.body</Trans>}
               ghLink="https://github.com/ThomasRitaine"
+              demoLink="https://cdn.thomas.ritaine.com"
             />
           </Col>
 
@@ -59,7 +62,17 @@ function Projects() {
               isBlog={false}
               title={<Trans>projects.terminal.title</Trans>}
               description={<Trans>projects.terminal.body</Trans>}
-              ghLink="https://github.com/ThomasRitaine/ThomasRitaine/tree/main/bash_custom"
+              ghLink="https://github.com/ThomasRitaine/terminal"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={server}
+              isBlog={false}
+              title={<Trans>projects.vps.title</Trans>}
+              description={<Trans>projects.vps.body</Trans>}
+              ghLink="https://github.com/ThomasRitaine/server-config"
             />
           </Col>
 
@@ -74,6 +87,15 @@ function Projects() {
             />
           </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pipeline}
+              isBlog={false}
+              title={<Trans>projects.ci-cd.title</Trans>}
+              description={<Trans>projects.ci-cd.body</Trans>}
+              ghLink="https://github.com/ThomasRitaine"
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
