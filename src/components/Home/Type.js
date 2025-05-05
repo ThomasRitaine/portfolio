3f487fe0b50aad5d7b1d56e9beca539567b52608
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-05-05 15:35:39 +0200

    feat(i18n): remove freelance option from home type strings

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -10,7 +10,6 @@ function Type() {
       options={{
         strings: [
           t('home.type.engineer'),
-          t('home.type.freelance'),
           t('home.type.devops'),
           t('home.type.opensource'),
         ],
