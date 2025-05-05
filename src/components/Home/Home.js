22b31c1b0517678a4f152fb9a7b84723a0d756fd
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-05-05 16:44:56 +0200

    feat(home): add call to action button to navigate to projects page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import homeLogo from "../../Assets/home-main.webp";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import { Trans, withTranslation } from "react-i18next";
+import { Trans, withTranslation, useTranslation } from "react-i18next";
 
 function Home() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(t('nav.projects.path'));
+  };
+
   return (
     <section>
       <Container fluid className="home-section" id="home">
@@ -28,6 +36,17 @@ function Home() {
 
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
+              </div>
+
+              <div className="text-center mb-4">
+                <Button
+                  variant="primary"
+                  size="lg"
+                  onClick={handleButtonClick}
+                  className="px-4 py-2"
+                >
+                  <Trans>home.view_projects</Trans>
+                </Button>
               </div>
             </Col>
 
