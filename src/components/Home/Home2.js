31a4830fe0e33867c7fb7a73babd414723b671b9
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-05-13 22:45:55 +0200

    feat(home): update content and structure of the about me section

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -8,64 +8,47 @@ import { Trans, withTranslation } from "react-i18next";
 import BottomNavigation from "../BottomNavigation";
 
 function Home2() {
-  function getAge(dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-    }
-    return age;
-  }
-
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              <Trans>home2.introduce.title</Trans>
+              <Trans>home2.about.title</Trans>
             </h1>
             <p className="home-about-body">
-              <Trans>home2.introduce.body.part1</Trans>
-              {getAge("2000-11-20") + 1}
-              <Trans>home2.introduce.body.part2</Trans>
+              <Trans
+                components={{
+                  a: <a href="https://cryptea.thomas.ritaine.com/" target="_blank" rel="noreferrer" />
+                }}
+              >
+                home2.about.intro
+              </Trans>
               <br />
               <br />
-              <Trans>home2.introduce.body.early_programming</Trans>
-              <a href="https://cryptea.thomas.ritaine.com/" target="_blank" rel="noreferrer">
-                <Trans>home2.introduce.body.cryptea_link</Trans>
-              </a>
+              <Trans>home2.about.intro2</Trans>
               <br />
               <br />
-              <h3>
-                🌐 <Trans>home2.introduce.body.section1.title</Trans>
-              </h3>
-              <ul>
-                <li>
-                  <Trans>home2.introduce.body.section1.body.li1</Trans>
-                </li>
-                <li>
-                  <Trans>home2.introduce.body.section1.body.li2</Trans>
-                </li>
-                <li>
-                  <Trans>home2.introduce.body.section1.body.li3</Trans>
-                </li>
-              </ul>
               <br />
               <h3>
-                🤖 <Trans>home2.introduce.body.section2.title</Trans>
+                🌐 <Trans>home2.about.technical.title</Trans>
               </h3>
               <p>
-                <Trans>home2.introduce.body.section2.body</Trans>
+                <Trans>home2.about.technical.body</Trans>
               </p>
               <br />
               <h3>
-                🛠️ <Trans>home2.introduce.body.section3.title</Trans>
+                🤖 <Trans>home2.about.ai.title</Trans>
               </h3>
               <p>
-                <Trans>home2.introduce.body.section3.body</Trans>
+                <Trans>home2.about.ai.body</Trans>
+              </p>
+              <br />
+              <h3>
+                🛠️ <Trans>home2.about.infrastructure.title</Trans>
+              </h3>
+              <p>
+                <Trans>home2.about.infrastructure.body</Trans>
                 <br />
                 <a
                   href="http://thomrit4ylnmuwuby3mexep44adyx6algeurmf5lgl57pywprcgm6dqd.onion"
@@ -77,21 +60,35 @@ function Home2() {
               </p>
               <br />
               <h3>
-                📱 <Trans>home2.introduce.body.section4.title</Trans>
+                📱 <Trans>home2.about.mobile.title</Trans>
               </h3>
               <p>
-                <Trans>home2.introduce.body.section4.body</Trans>
+                <Trans>home2.about.mobile.body</Trans>
               </p>
               <br />
               <h3>
-                📚 <Trans>home2.introduce.body.section5.title</Trans>
+                📚 <Trans>home2.about.learning.title</Trans>
               </h3>
               <p>
-                <Trans>home2.introduce.body.section5.body</Trans>
+                <Trans>home2.about.learning.body</Trans>
+              </p>
+              <br />
+              <h3>
+                🚀 <Trans>home2.about.sparetime.title</Trans>
+              </h3>
+              <p>
+                <Trans>home2.about.sparetime.body</Trans>
+              </p>
+              <br />
+              <h3>
+                ✨ <Trans>home2.about.moreabout.title</Trans>
+              </h3>
+              <p>
+                <Trans>home2.about.moreabout.body</Trans>
               </p>
               <br />
               <br />
-              <Trans>home2.introduce.body.footer</Trans>
+              <Trans>home2.about.footer</Trans>
               <br />
             </p>
           </Col>
